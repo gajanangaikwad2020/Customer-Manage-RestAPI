1c77a635ed242b1502e14ed878e57e0b7bed7a39
Gajanan Gaikwad <gajanangaikwad2020@gmail.com>
2023-03-12 15:13:23 +0530

    Customer Commit

---
+++ src/main/java/com/custmanage/CustomerManageRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.custmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManageRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManageRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/custmanage/CustomerManageRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManageRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
